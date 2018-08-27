52aaa80b9d8ac5867e2633d3a9d48d0180a36c09
bujordet <mo.bujordet@gmail.com>
2018-08-27 19:06:35 +0200

    Add mail

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Velkommen</h1>
         </header>
+        <footer>morten@bujordet.com</footer>
       </div>
     );
   }
